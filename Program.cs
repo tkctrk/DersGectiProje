2da09f70acaad528a3b4925237c00ecf51136ce1
tkctrk <143005369+tkctrk@users.noreply.github.com>
2023-08-23 17:32:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖĞRENCİ_DERS_GECME_PROJESİ
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("MATEMATİK NOTUNUZU GİRİNİZ");
+            double matnot = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("FİZİK NOTUNUZU GİRİNİZ");
+            double fiziknot = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("BEDEN NOTUNUZU GİRİNİZ");
+            double bedennot = Convert.ToDouble(Console.ReadLine());
+
+            List<string> ifadelist = new List<string>();
+
+            double matgecmenot = new Matematik().GecmeHesapla();           
+            string Matİfade = matnot > matgecmenot ? "MATEMATİKTEN GECTİNİZ" : "MATEMATİKTEN KALDINIZ";
+            ifadelist.Add(Matİfade);
+            double fizikgecmenot= new Fizik().GecmeHesapla();
+            string Fizikİfade = fiziknot > fizikgecmenot ? "FİZİKTEN GEÇTİNİZ" : "FİZİKTEN KALDINIZ";
+            ifadelist.Add(Fizikİfade);
+            double bedengecmenot = new Beden().GecmeHesapla();
+            string bedenİfade = bedennot > bedengecmenot ? "BEDENDEN GECTİNİZ" : "BEDENDEN KALDINIZ";
+            ifadelist.Add(bedenİfade);
+
+            foreach (var item in ifadelist)
+            {
+                Console.WriteLine(item);
+            }
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ ogrenci.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ÖĞRENCİ_DERS_GECME_PROJESİ
+{
+
+    class Ders
+    {
+        private string dersAd;
+        private double gecmenot = 50;
+
+        public string DersAd
+        {
+            get { return dersAd; }
+            set { dersAd = value; }
+        }
+
+        public double GecmeNot
+        {
+            get { return gecmenot; }
+            set { gecmenot = value; }
+        }
+        public virtual double GecmeHesapla()
+        {
+            return GecmeNot * 1;
+        }        
+    }
+    class Matematik : Ders
+    {
+        public override double GecmeHesapla()
+        {
+            return GecmeNot *1.2;
+        }
+    }
+    class Fizik : Ders
+    {
+        public override double GecmeHesapla()
+        {
+            return GecmeNot*1.1;
+        }
+    }
+    class Beden : Ders
+    {
+        public override double GecmeHesapla()
+        {
+            return GecmeNot*0.9; 
+        }
+    }
+}
